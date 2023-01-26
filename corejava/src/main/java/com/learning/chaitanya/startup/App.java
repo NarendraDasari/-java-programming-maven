b172c4ead8a2ef88ef7cd666884b5765b7899ee4
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-25 20:01:59 -0600

    code refactoring.

--- corejava/src/main/java/com/learning/chaitanya/startup/App.java
+++ corejava/src/main/java/com/learning/chaitanya/startup/App.java
@@ -1,7 +1,5 @@
 package com.learning.chaitanya.startup;
 
-import com.learning.chaitanya.models.*;
-
 /**
  * Hello world!
  *
@@ -11,6 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Person obj = new Person();
     }
 }
