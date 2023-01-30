d890c61ceb124592b7fc60e7295b38cc755caed6
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-30 13:07:29 -0600

    basic program for arrays.

--- corejava/src/main/java/com/learning/chaitanya/arrays/ArraysTest.java
+++ corejava/src/main/java/com/learning/chaitanya/arrays/ArraysTest.java
@@ -3,7 +3,27 @@ package com.learning.chaitanya.arrays;
 public class ArraysTest {
 	public void testSimpleArrays()
 	{
-	System.out.println("Executing simple arrays program");	
+	//System.out.println("Executing simple arrays program");	
+		
+		String[] students=new String[4];
+		students[0]="chaitanya";
+		students[1]="nanduri";
+		students[2]="narendra";
+		students[3]="dasari";
+		
+		
+		
+		System.out.println("Welcome" + "," + students[0]);
+		System.out.println("Welcome" + "," + students[1]);
+		System.out.println("Welcome" + "," + students[2]);
+		System.out.println("Welcome" + "," + students[3]);
+		
+		
+		
+		
+		// Welcome, chaita
+		//
+		
 	}
 
 }
---
+++ corejava/src/main/java/com/learning/chaitanya/conditionalloops/ReplaceForWithWhile.java
@@ -0,0 +1,18 @@
+package com.learning.chaitanya.conditionalloops;
+
+public class ReplaceForWithWhile {
+	public void testWhileLoopInsteadOfFor(int maxcount)
+	{
+		
+		int counter=1;
+		while(counter<=maxcount)
+		{
+		System.out.println(counter + "." + "While loop instead of For ");
+		counter++;
+		}
+	}
+	
+	
+	
+
+}
