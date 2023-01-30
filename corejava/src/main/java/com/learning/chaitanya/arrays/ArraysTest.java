016e3d7f3e96dcfa616bfcb383fea5e80825655c
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-30 13:29:07 -0600

    using for loop in arrays/ArraysTest.java(dynamic).

--- corejava/src/main/java/com/learning/chaitanya/arrays/ArraysTest.java
+++ corejava/src/main/java/com/learning/chaitanya/arrays/ArraysTest.java
@@ -1,15 +1,28 @@
 package com.learning.chaitanya.arrays;
 
+import java.util.Scanner;
+
 public class ArraysTest {
 	public void testSimpleArrays()
 	{
 	//System.out.println("Executing simple arrays program");	
 		
 		String[] students=new String[4];
-		students[0]="chaitanya";
-		students[1]="nanduri";
-		students[2]="narendra";
-		students[3]="dasari";
+//		students[0]="chaitanya";
+//		students[1]="nanduri";
+//		students[2]="narendra";
+//		students[3]="dasari";
+		
+		System.out.println("Enter first value:");
+		Scanner scan=new Scanner(System.in);
+		students[0]=scan.next();
+		System.out.println("Enter Second value:");
+		students[1]=scan.next();
+		System.out.println("Enter 3rd value:");
+		students[2]=scan.next();
+		System.out.println("Enter 4th value:");
+		students[3]=scan.next();
+		
 		
 				
 		
