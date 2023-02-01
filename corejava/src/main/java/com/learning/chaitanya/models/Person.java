f79fb8292963b267e3495ea17512d688ad386606
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-02-01 13:12:34 -0600

    still working on dates.

--- corejava/src/main/java/com/learning/chaitanya/models/Person.java
+++ corejava/src/main/java/com/learning/chaitanya/models/Person.java
@@ -1,7 +1,12 @@
 package com.learning.chaitanya.models;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 public class Person {
@@ -20,18 +25,46 @@ public class Person {
 		// get todays date.
 		// if we can compare todays date with the converted date type we will 
 		// - get the difference between this two dates.
-		 SimpleDateFormat sdf=new SimpleDateFormat("MM/dd/yyyy");
+		 SimpleDateFormat sdf=new SimpleDateFormat("YYYY-MM-dd");
+		 try {
+			 String formattedDob  = dob.replaceAll("/", "-");
+			Date startDate = sdf.parse(formattedDob);
+			
+			Instant instant=startDate.toInstant();
+			ZonedDateTime z=instant.atZone(ZoneId.systemDefault());
+			LocalDate start_date=z.toLocalDate();
+			
+			start_date.getYear();
+			System.out.println("year:"+ start_date.getYear());
+			
+			
+			
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		// DateFormat.
+		 
 		 Date d=new Date();
+		 
+		// LocalDate startDate = LocalDate.parse(dob);
+
+		// System.out.println("year:" +d.getYear());
+		 System.out.println(d);
 		 System.out.println(sdf.format(d));
 		 
 		 LocalDate local=LocalDate.now();
-		 local.getYear();
+		  int year=local.getYear();
 		 
-		 local.getMonthValue();
+		int month= local.getMonthValue();
 		 
-		 local.getDayOfMonth();
+		 int day=local.getDayOfMonth();
 		 
-		 System.out.println(LocalDate.now());
+		System.out.println(LocalDate.now());
+		
+		//LocalDate lDate = LocalDate.
 		return 25;
 	}
 	
